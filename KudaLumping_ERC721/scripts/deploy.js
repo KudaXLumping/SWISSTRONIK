f1d068267017e5579499604f77e69fe7e6d81c7c
KudaXLumping <lumpingk257@gmail.com>
2024-07-27 03:12:21 +0700

    Commit

--- KudaLumping_ERC721/scripts/deploy.js
+++ KudaLumping_ERC721/scripts/deploy.js
@@ -2,14 +2,14 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
 
-  const MyNFT = await ethers.getContractFactory("MyNFT");
-  const myNFT = await MyNFT.deploy();
-  await myNFT.deployed();
+  const MyToken = await ethers.getContractFactory("MyToken");
+  const myToken = await MyToken.deploy(deployer.address);
+  await myToken.deployed();
 
-  console.log("MyNFT deployed to:", myNFT.address);
+  console.log("MyToken deployed to:", myToken.address);
 
   // Mint an NFT
-  const tx = await myNFT.mintTo(deployer.address);
+  const tx = await myToken.mintTo(deployer.address);
   await tx.wait();
 
   console.log(`Minted NFT to ${deployer.address}`);
